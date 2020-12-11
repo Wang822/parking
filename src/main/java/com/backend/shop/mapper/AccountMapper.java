8ca7a24a1ef44e519af6e993dc89028fe5eb02d8
ComposeC <dumeii@foxmail.com>
2020-12-11 10:46:53 +0800

    init

---
+++ src/main/java/com/backend/shop/mapper/AccountMapper.java
@@ -0,0 +1,9 @@
+package com.backend.shop.mapper;
+
+
+import com.backend.shop.pojo.Account;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface AccountMapper extends BaseMapper<Account> {
+
+}
---
+++ src/main/java/com/backend/shop/pojo/Account.java
@@ -0,0 +1,45 @@
+package com.backend.shop.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@Builder
+@TableName("account")
+public class Account {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "account_id", type = IdType.AUTO)
+    private int accountId;
+
+    @TableField("open_id")
+    private String openId;
+
+    @TableField("authenticated")
+    private boolean authenticated;
+
+    @TableField("nick_name")
+    private String nickName;
+
+    @TableField("create_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date createTime;
+
+    @TableField("last_visit_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date lastVisitTime;
+
+    @TableField("skey")
+    private String skey;
+
+    @TableField("session_key")
+    private String sessionKey;
+
+}
