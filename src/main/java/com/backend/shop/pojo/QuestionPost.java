e2853475ee999af0c807b0e172e093b8fa38d829
Wang822 <1377082895@qq.com>
2020-12-17 20:18:42 +0800

    QuestionPost

--- src/main/java/com/backend/shop/pojo/QuestionPost.java
+++ src/main/java/com/backend/shop/pojo/QuestionPost.java
@@ -1,76 +1,36 @@
 package com.backend.shop.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Date;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class QuestionPost {
 
-    private int q_post_id;
-    private int user_id;
-    private String q_content;
-    private String q_title;
+    @TableId(value = "q_post_id", type = IdType.AUTO)
+    private int qPostId;
+
+    @TableField("user_id")
+    private int userId;
+
+    @TableField("q_content")
+    private String qContent;
+
+    @TableField("q_title")
+    private String qTitle;
+
+    @TableField("time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private Date time;
 
-    public QuestionPost() {
-    }
-
-    public QuestionPost(int q_post_id, int user_id, String q_content, String q_title, Date time) {
-        this.q_post_id = q_post_id;
-        this.user_id = user_id;
-        this.q_content = q_content;
-        this.q_title = q_title;
-        this.time = time;
-    }
-
-    public int getQ_post_id() {
-        return q_post_id;
-    }
-
-    public void setQ_post_id(int q_post_id) {
-        this.q_post_id = q_post_id;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getQ_content() {
-        return q_content;
-    }
-
-    public void setQ_content(String q_content) {
-        this.q_content = q_content;
-    }
-
-    public String getQ_title() {
-        return q_title;
-    }
-
-    public void setQ_title(String q_title) {
-        this.q_title = q_title;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    @Override
-    public String toString() {
-        return "QuestionPost{" +
-                "q_post_id=" + q_post_id +
-                ", user_id=" + user_id +
-                ", q_content='" + q_content + '\'' +
-                ", q_title='" + q_title + '\'' +
-                ", time=" + time +
-                '}';
-    }
 }
 
