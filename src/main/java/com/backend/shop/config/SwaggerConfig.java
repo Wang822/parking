d65df7a12faf97e66a3339ff959fa5589578e3db
Zihan <azphire@163.com>
2020-12-17 12:13:45 +0800

    配置Swagger

---
+++ src/main/java/com/backend/shop/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package com.backend.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"REST API 测试\"")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.backend.shop.controller"))
+                .paths(regex("/.*"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
--- src/main/java/com/backend/shop/controller/ChatController.java
+++ src/main/java/com/backend/shop/controller/ChatController.java
@@ -5,6 +5,9 @@ import com.backend.shop.pojo.Chat;
 import com.backend.shop.pojo.User;
 import com.backend.shop.service.ChatService;
 import com.backend.shop.util.TokenUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
+@Api(tags = "chat")
 @RequestMapping("/chat")
 public class ChatController {
 
@@ -31,9 +35,11 @@ public class ChatController {
      * @throws ParseException time pattern error
      */
     @PostMapping("/addMessage")
-    public GlobalResult addChat(@RequestHeader(value = "Authorization") String token, @RequestParam int receiver_id,
-                                @RequestParam String content,
-                                @RequestParam String time) throws ParseException {
+    @ApiOperation(value = "add a new massage")
+    public GlobalResult addChat(@RequestHeader(value = "Authorization") String token,
+                                @ApiParam("receiver's id") @RequestParam int receiver_id,
+                                @ApiParam("message's content") @RequestParam String content,
+                                @ApiParam("time, yyyy-MM-dd hh:mm:ss") @RequestParam String time) throws ParseException {
         int userId = TokenUtil.getUserId(token);
         Chat chat = new Chat();
         chat.setReceiverId(receiver_id);
@@ -53,9 +59,10 @@ public class ChatController {
         return new GlobalResult(200, "successful operation", users);
     }
 
+    @ApiOperation(value = "get all messages between two users", response = Chat.class)
     @GetMapping("getMessages/{anotherUserId}")
     public GlobalResult getMessages(@RequestHeader(value = "Authorization") String token,
-                                    @PathVariable int anotherUserId) {
+                                    @ApiParam("another user's id") @PathVariable int anotherUserId) {
         int userId = TokenUtil.getUserId(token);
         ArrayList<Chat> chats = chatService.selectMessages(userId, anotherUserId);
 //        if(chats.isEmpty()){
