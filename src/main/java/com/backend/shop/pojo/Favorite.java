d71ba4a1401c2e0cc703756e2a4a423bbe2a5d35
Zihan <azphire@163.com>
2020-12-19 23:21:17 +0800

    添加了example注解

--- src/main/java/com/backend/shop/pojo/Favorite.java
+++ src/main/java/com/backend/shop/pojo/Favorite.java
@@ -1,6 +1,7 @@
 package com.backend.shop.pojo;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,10 @@ import lombok.NoArgsConstructor;
 public class Favorite {
 
     @TableField("user_id")
+    @ApiModelProperty(value = "user's id", example = "6")
     private int userId;
 
     @TableField("good_id")
+    @ApiModelProperty(value = "good's id", example = "10")
     private int goodId;
 }
