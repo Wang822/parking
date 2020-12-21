79ab9b95c78050437717f3c70472540e8edc4793
Firstname Lastname <43996310+DeBroglie99@users.noreply.github.com>
2020-12-21 22:22:58 +0800

    delete TJShop

--- TJShop/src/main/java/com/backend/shop/mapper/AccountMapper.java
+++
@@ -1,9 +0,0 @@
-package com.backend.shop.mapper;
-
-
-import com.backend.shop.pojo.Account;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface AccountMapper extends BaseMapper<Account> {
-
-}
