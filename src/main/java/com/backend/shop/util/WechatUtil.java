187b5e5a413f577131d9575ee4fd9692dae41bc1
Zihan <azphire@163.com>
2021-01-10 16:40:31 +0800

    去掉了小程序登录信息

--- src/main/java/com/backend/shop/util/WechatUtil.java
+++ src/main/java/com/backend/shop/util/WechatUtil.java
@@ -25,9 +25,9 @@ public class WechatUtil {
         Map<String, String> requestUrlParam = new HashMap<>();
         // https://mp.weixin.qq.com/wxopen/devprofile?action=get_profile&token=164113089&lang=zh_CN
         //小程序appId
-        requestUrlParam.put("appid", "wx5482420b2aad957a");
+        requestUrlParam.put("appid", "");
         //小程序secret
-        requestUrlParam.put("secret", "c473847a1eb07cef765b0e9c664eda00");
+        requestUrlParam.put("secret", "");
         //小程序端返回的code
         requestUrlParam.put("js_code", code);
         //默认参数
